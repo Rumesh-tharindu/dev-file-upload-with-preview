3fbe7dc7877d765ea9e2729edc834277024ed9df
amarsinghe <rumesh@silverstripers.com>
2023-11-12 22:33:36 +0530

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
-	console.log('hi there');
+	const fileInput = $('#fileInput');
+	const filesWrapper = $('.files');
+
+	fileInput.on('change', function () {
+		let fileName = $(this).val().split('\\').pop();
+		filesWrapper.append(
+			`<div class='file'>
+        <div class="fileLeft">
+          <img src='./assets/images/pages/fileupload/file.png' />
+          <p>  ${fileName}</p>
+        </div>
+        .<button class="fileAction">
+          <img src='./assets/images/icons/cross-icon.png' />
+        </button>
+            
+           </div>`
+		);
+	});
 });
