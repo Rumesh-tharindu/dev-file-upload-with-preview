21c5bf63622bacae84856f4a23020422d6d0f47c
amarsinghe <rumesh@silverstripers.com>
2023-11-17 23:17:56 +0530

    get to starter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,3 @@
 $(document).ready(function () {
-	const fileInput = $('#fileInput');
-	const filesWrapper = $('.files');
-
-	fileInput.on('change', function () {
-		let fileName = $(this).val().split('\\').pop();
-		filesWrapper.html(
-			`<div class='file'>
-        <div class="fileLeft">
-          <img src='./assets/images/pages/fileupload/file.png' />
-          <p>${fileName}</p>
-        </div>
-        .<button class="fileAction">
-          <img src='./assets/images/icons/cross-icon.png' />
-        </button>
-            
-           </div>`
-		);
-
-		// If you want to make on method to attach handlers to elements dynamically created, you should use it like this:
-
-		$('.file').on('click', '.fileAction', function (ev) {
-			$(this).parent().remove();
-		});
-	});
+	console.log('Hey this my main js file');
 });
